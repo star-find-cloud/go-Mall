65722a89b6a922ab06b656f78acf184d7a093993
star-find-cloud <2958268952@qq.com>
2025-05-14 22:16:01 +0800

    update file

--- conf/config_test.go
+++ conf/config_test.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -10,8 +11,8 @@ func TestGetConfig(t *testing.T) {
 	if c == nil {
 		t.Fatal("配置对象为 nil，初始化失败")
 	}
-	//a := os.Getenv("JWT_SECRET")
-	//fmt.Println(a)
+	a := os.Getenv("JWT_SECRET")
+	fmt.Println(a)
 
 	// 打印完整结构体（调试用）
 	fmt.Println(c.App)
